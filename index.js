bca23a19b7a76aa2acb119891f5edd2eb2c66b1b
Unknown <ro0504008@otc.edu>
2017-09-06 13:32:48 -0500

    old man song :)

---
+++ index.js
@@ -0,0 +1,33 @@
+const chalk = require('chalk');
+ 
+console.log(chalk.bgWhite.blue('HAPPY ', chalk.bgBlue.red('BIRTH ') + chalk.bgRed.white('DAY') + chalk.bgYellow.redBright(" ADAM ENGEBRETSON")));
+console.log(chalk.red('Once a year we celebrate with stupid hats and plastic plates,'));
+console.log(chalk.green('the fact that you were able to make another trip around the sun.'));
+console.log(chalk.yellow('And the whole plan gathers round gifts and laughter do will bound,'));
+console.log(chalk.blue('we let out a joyful sound and sing that stupid song.'));
+console.log(chalk.magenta('Happy Birthday, now your one year older.'));
+console.log(chalk.cyan('Happy Birthday, now your one year older.'));
+console.log(chalk.white('Happy Birthday, your life still is not over.'));
+console.log(chalk.redBright('Happy Birthday, you did not accomplish much.'));
+console.log(chalk.greenBright('But you did not die this year i guess that is good enough.'));
+console.log(chalk.yellowBright('So lets drink to your fading health, and hope you do not remind yourself  '));
+console.log(chalk.magenta('your chance of finding fame and wealth decrease with every year.'));
+console.log(chalk.magentaBright('Does it feel like your doing laps, '));
+console.log(chalk.cyanBright('and eating food and taking naps,'));
+console.log(chalk.whiteBright('and hoping that some day, perhaps, your life will hold some cheer.'));
+console.log(chalk.yellowBright('Happy Birthday, what have you done that matters?'));
+console.log(chalk.red('Happy Birthday, your starting to get fatter.'));
+console.log(chalk.blueBright('Happy Birthday, it is downhill from now on.'));
+console.log(chalk.cyanBright('Try not to remind yourself your best years are all gone.'));
+console.log(chalk.magenta('If cryogenics were all free then you could live like Walt Disney and live'));
+console.log(chalk.red('for all eternity inside a block of ice.'));
+console.log(chalk.yellowBright('But instead your time is set this is the only life you get,'));
+console.log(chalk.blueBright('and though it has not ended yet some times you wish it MIGHT!'));
+console.log(chalk.magenta('Happy Birthday, you wish you had more money.'));
+console.log(chalk.redBright('Happy Birthday, your lifes so sad it is funny.'));
+console.log(chalk.blue('Happy Birthday, how much more can you take?'));
+console.log(chalk.magenta('But your friends are hungry so just cut the stupid cake.'));
+console.log(chalk.yellowBright('Happy Birthday, Happy Birthday, Happy Birthday, Dear') + chalk.bgWhite.redBright(' ADAM!!!!!!'));
+console.log(chalk.bgWhite.blue('HOPE', chalk.bgBlue.red(' YOU ') + chalk.bgRed.white('HAVE')+(chalk.bgWhite.blue(' A ', chalk.bgBlue.red('GREAT') + chalk.bgRed.white(' DAY ')+(chalk.bgWhite.blue('OLD', chalk.bgBlue.red(' MAN ') + chalk.bgRed.white('!!!!!') + ":p"))))));
+
+
